d29ada33b867c55661aea911c766c5d91faf2e5d
AMO15310 <wachiraamos402@gmail.com>
2023-05-04 07:49:38 +0300

    Watch school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and pass it to a regular expression matching method.
 
-puts ARGV[0].scan(/school/i).join
+regex = /School/
+input = ARGV[0]
+
+if input.match(regex)
+  puts input.match(regex)
+else
+  puts ""
+end
